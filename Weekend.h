2656fb37dc5e0c74336b41529899371c0f756ad0
dogman0211 <dogman0211@gmail.com>
2025-09-06 14:50:03 +0900

    헤더 위에 주석으로 설명 표시

--- Weekend.h
+++ Weekend.h
@@ -1,11 +1,19 @@
 #pragma once
 
+// 3개의 파라메터를 받아 평균값을 리턴하는 함수 만들기
 float AvarageReturn(int Number1, int Number2, int Number3);
+
+// 정가와 할인율 입력 받아 할인가 구하는 함수 만들기
 float PriceReturn(float OriginPrice, float RatePrice);
+
+// 주사위를 굴린 결과를 리턴하는 함수 만들기
 int PlayDice();
+
+// 점수를 주면 성적(A~F)를 주는 함수 만들기
 int ReturnGradeScore(int GradeScore);
 
 template <typename T>
+// 템플릿 함수로 Clamp 함수 구현하기
 T Clamp(T value, T min, T max)
 {
 	if ((value - min) == (max - value)) {
